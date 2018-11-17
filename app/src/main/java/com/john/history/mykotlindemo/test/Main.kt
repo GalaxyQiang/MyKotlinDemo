82f6b5007036765990f782683adec52738512fd4
Administrator <galaxyqiang@gmail.com>
2018-11-17 18:49:26 +0800

    调用扩展属性

--- app/src/main/java/com/john/history/mykotlindemo/test/Main.kt
+++ app/src/main/java/com/john/history/mykotlindemo/test/Main.kt
@@ -1,21 +1,28 @@
 package com.john.history.mykotlindemo.test
 
+import android.arch.lifecycle.MutableLiveData
+
 fun main(args: Array<String>) {
     val xiaoming=Student("high school","小明",16)
     val xiaohong=Student("college","小红",20)
-    println(xiaoming.name+", "+xiaoming.age+", "+xiaoming.category)
-    println(xiaohong.name+", "+xiaohong.age+", "+xiaohong.category)
-    xiaoming.sex="male"
-    xiaohong.sex="female"
-    println(xiaoming.name+", "+xiaoming.age+", "+xiaoming.category)
-    println(xiaohong.name+", "+xiaohong.age+", "+xiaohong.category+", "+xiaohong.sex)
+//    println(xiaoming.name+", "+xiaoming.age+", "+xiaoming.category)
+//    println(xiaohong.name+", "+xiaohong.age+", "+xiaohong.category)
+//    xiaoming.sex="male"
+//    xiaohong.sex="female"
+//    println(xiaoming.name+", "+xiaoming.age+", "+xiaoming.category)
+//    println(xiaohong.name+", "+xiaohong.age+", "+xiaohong.category+", "+xiaohong.sex)
+    val data=MutableLiveData<String>()
+    data.value="kotlin demo"
+    println(xiaoming.sex)
+
 }
 
 open class People( name:String,var age:Int){
-  var name:String
+  var name:String="People init执行之前的name=$name".also { println(it) }
     get() = field
     init {
         this.name=name
+        println(name)
 
     }
 }
