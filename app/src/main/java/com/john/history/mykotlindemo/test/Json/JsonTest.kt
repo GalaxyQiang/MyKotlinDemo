5df12da3b1e694b810defd45ab623daefbadec18
John <galaxyqiang@gmail.com>
2019-05-31 05:55:41 +0800

    fullscreen

---
+++ app/src/main/java/com/john/history/mykotlindemo/test/Json/JsonTest.kt
@@ -0,0 +1,23 @@
+package com.john.history.mykotlindemo.test.Json
+
+import android.arch.lifecycle.MediatorLiveData
+import com.google.gson.Gson
+import java.io.*
+
+fun main() {
+    val file=File("D:/json2.txt")
+    var student=Student("john","male",31 )
+//    var mediatorLiveData=MediatorLiveData<Double>()
+   var string=Gson().toJson(student)
+    println(string)
+//    var fos=FileOutputStream(file).write(string.toByteArray())
+    var fos=BufferedWriter(OutputStreamWriter(FileOutputStream(file)))
+    fos.write(string)
+    fos.close() //不写这段代码，保存的文件内容是空的
+}
+
+class Student( var name:String,  var sex:String,  var age:Int):Teacher(name,sex,age){
+
+}
+
+open class Teacher( var name1:String, var sex1:String, var age1:Int=50){}
---
+++ app/src/main/java/com/john/history/mykotlindemo/test/Unitvoidnull.kt
@@ -0,0 +1,16 @@
+package com.john.history.mykotlindemo.test
+
+fun main() {
+    var a:Int?=10
+
+    println(aaa())
+    println(a)
+}
+val bbb={
+    var a=100
+    a=200
+}
+val aaa= {
+    println("run foo")//java方法无返回值，void
+
+}
